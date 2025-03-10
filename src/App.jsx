59d5581af4912750326cc566432ac4581a59f598
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2025-03-10 11:22:52 +0100

    Finition de la barre de navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Navbar } from './components/navbar'
+import { Header } from './components/header'
 
 function App() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   )
 }
