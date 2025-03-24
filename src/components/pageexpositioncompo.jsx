1775d55b1f22b168e1f76208523005679c56986a
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2025-03-24 09:21:03 +0100

    ajout du lien du site immersive 3D

--- src/components/pageexpositioncompo.jsx
+++ src/components/pageexpositioncompo.jsx
@@ -28,7 +28,7 @@ export const Expopage = () => {
             <p>
                 <Trans i18nKey="expotext"></Trans>
             </p>
-            <a href="#"><i className="fa-solid fa-gamepad"></i> {t('jouermtn')}</a>
+            <a href="https://molduch.butmmi.o2switch.site/site_3D/" target='blank'><i className="fa-solid fa-gamepad"></i> {t('jouermtn')}</a>
             <img src="./img/expo/illustrationVR.png" alt="" />
         </header>
     );
